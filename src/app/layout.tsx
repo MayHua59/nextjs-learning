f6b0d94fe5a2abe38a4fb12c99fe78912d6468ef
MayHua59 <mayhuadev59@gmail.com>
2025-09-04 13:16:57 +0630

    lesson 16 Multiple Root Layouts

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+
+
+import type { ReactNode } from 'react';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>Header</header>
+        {children}
+        <footer>Footer</footer>
+      </body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-import Link from "next/link"
-
 export default function Home(){
-    return <div>
-        <Link href={"/knowledge-base/getting-started/installation"}>Installing</Link>
-        <Link href={"/knowledge-base/advanced-topics/security/best-practices"}>Best Practices</Link>
-    </div>
+    return <h1>Home page</h1>
 }
